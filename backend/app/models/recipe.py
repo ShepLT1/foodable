345efa05e485589655871a357da0b259199ecbdf
Jose Renteria <jose.renteriaq@gmail.com>
2026-07-17 23:36:52 -0700

    Add user_id foreign key to Recipe model

--- backend/app/models/recipe.py
+++ backend/app/models/recipe.py
@@ -1,8 +1,10 @@
 from uuid import UUID, uuid4
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
 
 from app.db.base import Base
+
 
 class Recipe(Base):
     __tablename__ = "recipes"
@@ -11,4 +13,10 @@ class Recipe(Base):
         PG_UUID(as_uuid=True),
         primary_key=True,
         default=uuid4,
+    )
+
+    user_id: Mapped[UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
+        ForeignKey("profiles.id", ondelete="CASCADE"),
+        nullable=False,
     )
