03e38f5f5dd56e51f6c38cbfc2476310ccedd6f1
ShepLT1 <luke.t.shepherd@gmail.com>
2026-07-18 18:34:37 -0700

    Move grocery list item created_at and index to new migration

--- supabase/migrations/20260716000750_add_timestamps_to_list_tables.sql
+++ supabase/migrations/20260716000750_add_timestamps_to_list_tables.sql
@@ -1,10 +1,3 @@
 -- grocery_lists
 alter table public.grocery_lists
 add column updated_at timestamptz not null default now();
-
---grocery_list_items
-alter table public.grocery_list_items
-add column created_at timestamptz not null default timezone('utc', now());
-
-create index idx_grocery_list_items_created_at
-on public.grocery_list_items (created_at);
