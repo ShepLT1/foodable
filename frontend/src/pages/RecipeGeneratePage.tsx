0fa298d03627d3d62ef6b273c45eade5aa014bc3
Jose Renteria <jose.renteriaq@gmail.com>
2026-07-20 21:54:58 -0700

    Add onError alert for failed recipe generation

--- frontend/src/pages/RecipeGeneratePage.tsx
+++ frontend/src/pages/RecipeGeneratePage.tsx
@@ -36,6 +36,9 @@ export function RecipeGeneratePage() {
         onSuccess: (recipe) => {
           navigate(`/recipes/${recipe.id}`, { state: { recipe } })
         },
+        onError: () => {
+          alert('Something went wrong, please try again')
+        },
       },
     )
   }
