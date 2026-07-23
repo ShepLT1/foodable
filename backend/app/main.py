853f31fa76798d9abb88103af53f8545a80f16de
ShepLT1 <luke.t.shepherd@gmail.com>
2026-07-23 16:28:57 -0700

    Add meal plan routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from app.api.routes.health import router as health_router
 from app.api.routes.lists import router as lists_router
 from app.api.routes.recipes import router as recipes_router
 from app.api.routes.users import router as users_router
+from app.api.routes.meal_plans import router as meal_plans_router
 from app.middleware.cors import add_cors
 
 app = FastAPI(
@@ -17,3 +18,4 @@ app.include_router(health_router)
 app.include_router(users_router)
 app.include_router(lists_router)
 app.include_router(recipes_router)
+app.include_router(meal_plans_router)
