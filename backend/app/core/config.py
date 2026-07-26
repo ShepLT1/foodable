66c6e0d8e795db9e551bba34f8c5b377a07c07d6
efanibi25 <58971180+efanibi25@users.noreply.github.com>
2026-07-26 16:13:03 -0500

    chore: fix fallback

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 
 class Settings:
-    supabase_url: str = os.environ.get["SUPABASE_URL"]
+    supabase_url: str = os.environ["SUPABASE_URL"]
     supabase_publishable_key: str = os.environ["SUPABASE_PUBLISHABLE_KEY"]
     database_url: str = os.environ["DATABASE_URL"]
-    frontend_url: str = os.environ.get["FRONTEND_URL"]
+    frontend_url: str = os.environ["FRONTEND_URL"]
 
 
 settings = Settings()
